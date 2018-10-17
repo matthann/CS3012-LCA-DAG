efc4b2569e88e08098114d790a624c0b0a36d253
Hannah Matthews <hslmatthews@gmail.com>
2018-10-17 18:06:33 +0100

    Created class Tree and a constructor for the class representing an instance of the object itself. Declared function put to insert nodes with specific values for the tree.

--- LCA.py
+++ LCA.py
@@ -1,7 +1,42 @@
+
+# Assignment: Given a Binary Tree and two inputs. Write a program to find their lowest common ancestor.
+
 class Node(object):
 
     # Constructor
-    def__init__(self, value):
-    self.val = value
+    def__init__(self, val):
+    self.val = val
     self.left = None
     self.right = None
+
+    def __lt__(self,val):
+        return self.val < val
+
+    def __gt__(self,val):
+        return self.val > val
+
+    def __eq__(self,val):
+        return self.val == val
+
+
+class Tree(object):
+
+    def__init__(self):
+    self.root = None
+
+    def put(self,val):
+        if not (self.node_exists(val)):
+            self.root = self._put(self.root,val)
+
+    def _put(self,node,val):
+        if node is None:
+            node = Node(val)
+
+        if val < node:
+            node.left = self._put(self.root,val)
+        elif val > node:
+            node.right = self._put(node.right,val)
+        else:
+            node.val = val
+
+        return node
